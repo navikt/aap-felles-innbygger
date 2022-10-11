e6633bc9e3a57d4642e472a80e95e25b5aae89d9
Tor Idland <tor.idland@nav.no>
2022-10-11 14:37:33 +0200

    :green_heart: Named export på logger

--- packages/aap-felles-innbygger-utils/src/logger/logger.ts
+++ packages/aap-felles-innbygger-utils/src/logger/logger.ts
@@ -1,6 +1,5 @@
 import pino from 'pino';
 import ecsFormat from '@elastic/ecs-pino-format';
 
-const ecsLogger = pino(ecsFormat());
+export const logger = pino(ecsFormat());
 
-export default ecsLogger;
