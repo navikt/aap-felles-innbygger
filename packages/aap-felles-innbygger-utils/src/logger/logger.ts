7a094d7550a019c0080839775026f26560624618
Tor Idland <tor.idland@nav.no>
2022-10-28 08:17:09 +0200

    :wrench: log level

--- packages/aap-felles-innbygger-utils/src/logger/logger.ts
+++ packages/aap-felles-innbygger-utils/src/logger/logger.ts
@@ -1,5 +1,11 @@
 import pino from 'pino';
 import ecsFormat from '@elastic/ecs-pino-format';
 
-export const logger = pino(ecsFormat());
-
+export const logger = pino({
+    ...ecsFormat(),
+    formatters: {
+        level: (label) => {
+            return { level: label };
+        },
+    },
+});
