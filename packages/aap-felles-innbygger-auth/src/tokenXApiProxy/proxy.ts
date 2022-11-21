7feb5c1172ee164e6e9db7b2e602f520d087f26e
tidnav <90849097+tidnav@users.noreply.github.com>
2022-11-21 14:05:41 +0100

    :sparkles: tokenApiProxy i auth (#5)

---
+++ packages/aap-felles-innbygger-auth/src/tokenXApiProxy/proxy.ts
@@ -0,0 +1,155 @@
+import { randomUUID } from 'crypto';
+import { NextApiRequest, NextApiResponse } from 'next/dist/shared/lib/utils';
+import axios from 'axios';
+import { getTokenX } from '@navikt/aap-felles-innbygger-auth';
+import { ErrorMedStatus } from '../lib/ErrorMedStatus';
+import { Counter, Histogram } from 'prom-client';
+import pino from 'pino';
+import Logger = pino.Logger;
+
+const NAV_CALLID = 'Nav-CallId';
+interface ErrorLog {
+  status?: number;
+  message: string;
+  [NAV_CALLID]?: string;
+  error?: string;
+  data?: string;
+}
+interface Opts {
+  url: string;
+  prometheusPath: string;
+  audience: string;
+  method: 'GET' | 'POST' | 'DELETE';
+  data?: string;
+  req?: NextApiRequest;
+  contentType?: string;
+  rawResonse?: boolean;
+  noResponse?: boolean;
+  bearerToken?: string;
+  metricsTimer?: Histogram;
+  metricsStatusCodeCounter?: Counter;
+  logger: Logger;
+}
+
+export const tokenXApiProxy = async (opts: Opts) => {
+  opts.logger.info('starter request mot ' + opts.url);
+
+  const idportenToken = opts.bearerToken!.split(' ')[1];
+  let tokenxToken;
+  try {
+    tokenxToken = await getTokenX(idportenToken, opts.audience);
+  } catch (err: any) {
+    opts.logger.error({ msg: 'getTokenXError', error: err });
+  }
+
+  const stopTimer = opts.metricsTimer
+    ? opts.metricsTimer.startTimer({ path: opts.prometheusPath })
+    : () => {};
+  const requestId = randomUUID();
+  const response = await fetch(opts.url, {
+    method: opts.method,
+    body: opts.data,
+    headers: {
+      Authorization: `Bearer ${tokenxToken}`,
+      'Content-Type': opts.contentType ?? 'application/json',
+      [NAV_CALLID]: requestId,
+    },
+  });
+  stopTimer();
+  opts.metricsStatusCodeCounter &&
+    opts.metricsStatusCodeCounter.inc({ path: opts.prometheusPath, status: response.status });
+
+  if (response.status < 200 || response.status > 300) {
+    const headers = response.headers.get('content-type');
+    const isJson =
+      headers?.includes('application/json') || headers?.includes('application/problem+json');
+    let data;
+    try {
+      data = isJson ? await response.json() : response.text();
+    } catch (err: any) {
+      const parseError: ErrorLog = {
+        message: `unable to parse data from ${opts.url}`,
+        error: err.toString(),
+      }
+      opts.logger.error(parseError);
+    }
+    const responseErrorLog: ErrorLog = {
+      message: `tokenXProxy: status for ${opts.url} er ${response.status}: ${response.statusText}.`,
+      [NAV_CALLID]: data?.[NAV_CALLID],
+      data,
+    }
+    if (response.status >= 500 || response.status === 400) {
+      opts.logger.error(responseErrorLog);
+    } else {
+      opts.logger.warn(responseErrorLog);
+    }
+    throw new ErrorMedStatus(
+      `tokenXProxy: status for ${opts.url} er ${response.status}.`,
+      response.status,
+      data?.[NAV_CALLID]
+    );
+  }
+  opts.logger.info(`Vellyket tokenXProxy-request mot ${opts.url}. Status: ${response.status}`);
+  if (opts.noResponse) {
+    return;
+  }
+  if (opts.rawResonse) {
+    return response;
+  }
+  return await response.json();
+};
+
+interface AxiosOpts {
+  url: string;
+  prometheusPath: string;
+  audience: string;
+  req: NextApiRequest;
+  res: NextApiResponse;
+  bearerToken?: string;
+  metricsTimer?: Histogram;
+  metricsStatusCodeCounter?: Counter;
+  logger: Logger;
+}
+
+export const tokenXApiStreamProxy = async (opts: AxiosOpts) => {
+  const idportenToken = opts.bearerToken!.split(' ')[1];
+  const tokenxToken = await getTokenX(idportenToken, opts.audience);
+
+  opts.logger.info('Starter opplasting av fil til ' + opts.url);
+  opts.logger.info('content-type fra klient' + opts.req?.headers['content-type']);
+  const requestId = randomUUID();
+  try {
+    const stopTimer = opts.metricsTimer
+      ? opts.metricsTimer.startTimer({ path: opts.prometheusPath })
+      : () => {};
+    const { data } = await axios.post(opts.url, opts.req, {
+      responseType: 'stream',
+      headers: {
+        'Content-Type': opts.req?.headers['content-type'] ?? '', // which is multipart/form-data with boundary included
+        Authorization: `Bearer ${tokenxToken}`,
+        [NAV_CALLID]: requestId,
+      },
+    });
+    stopTimer();
+    opts.metricsStatusCodeCounter &&
+      opts.metricsStatusCodeCounter.inc({ path: opts.prometheusPath, status: data.status });
+    opts.logger.info('Vellykket opplasting av fil til ' + opts.url);
+    return data.pipe(opts.res);
+  } catch (e: any) {
+    if (e?.response?.status) {
+      e.response.data?.pipe(opts.res);
+      opts.metricsStatusCodeCounter &&
+        opts.metricsStatusCodeCounter.inc({
+          path: opts.prometheusPath,
+          status: e.response.status,
+        });
+      return opts.res.status(e.response.status);
+    }
+    opts.logger.error({
+      msg: 'tokenXAxioserror',
+      error: e.toString(),
+      navCallId: e?.request?.headers?.[NAV_CALLID],
+    });
+    return opts.res.status(500).json('tokenXAxiosProxy server error');
+  }
+};
